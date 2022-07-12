a7cba1686b0d82e432e0967af8b16265d7928090
Patrick Poopathi <patrickgp1992@hotmail.com>
2022-07-11 20:43:43 -0400

    fixed redirect

--- assets/js/webapp.js
+++ assets/js/webapp.js
@@ -320,7 +320,7 @@ function clearHistory(event) {
   savedCities = [];
   localStorage.removeItem("cityname");
   document.location.reload();
-  location.href = ".https://patrickgp.github.io/Spotify-Weather-App/";
+  location.href = "";
 }
 
 // Tie weather condition with playlist options
